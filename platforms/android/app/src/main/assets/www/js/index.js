8c4e874f27eceada588f30924a0c379c2b190d40
PHANTOM-NZXT\Kevin <kevin@online123.be>
2018-05-06 15:36:06 +0200

    start

---
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -0,0 +1,50 @@
+cordova.plugins.barcodeScanner.scan(
+        function (result) {
+            if (!result.cancelled)
+            {
+                alert("Barcode type is: " + result.format);
+                alert("Decoded text is: " + result.text);
+            } else
+            {
+                alert("You have cancelled scan");
+            }
+        },
+        function (error) {
+            alert("Scanning failed: " + error);
+        }
+);
+
+
+
+
+var app = {
+    // Application Constructor
+    initialize: function () {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function () {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function () {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function (id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
